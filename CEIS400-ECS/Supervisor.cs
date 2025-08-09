ffc379a3ede72be3f7995e91ff2684fbc8fc310c
schefco <c.scheffler988@gmail.com>
2025-08-09 11:11:28 -0400

    Here are the updated classes so far. ITrackable now has a base class Trackable for the other tool classes. Employee is the base class for the Role classes. Added an Enum class for Roles to make validation easier to work with.

---
+++ CEIS400-ECS/Supervisor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEIS400_ECS
+{
+    public class Supervisor : Employee
+    {
+        public Supervisor(string empID, string name, string email, string phone, EmpStatus status, string title, string passwordHash, string passwordSalt)
+            : base(empID, name, email, phone, status, title, passwordHash, passwordSalt, Roles.Supervisor)
+        {
+
+        }
+
+        private void IsSupervisor()
+        {
+            if (Role != Roles.Supervisor)
+            {
+                throw new UnauthorizedAccessException("Only a supervisor can perform this action");
+            }
+        }
+
+        public void GenerateReports()
+        {
+
+        }
+
+        public void PlaceHold() 
+        {
+            IsSupervisor();
+            // place holds on accounts
+        }
+
+        public void AssignCerts()
+        {
+            IsSupervisor();
+            // Assign Certs to customer accounts
+        }
+
+        public void UpdateCerts()
+        {
+            IsSupervisor();
+            // Update certs to customer accounts. Cert could be suspended or pending.
+        }
+
+        public void RevokeCerts()
+        {
+            IsSupervisor();
+            // Delete cert from customer account
+        }
+    }
+}
